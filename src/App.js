bef042d9f5cf3ce902429ed8c019ad91995fc5e3
Mehedi4467 <mehedi446768@gmail.com>
2022-05-23 05:29:45 +0600

    menu bar complated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./Pages/Footer/Footer";
+import Header from "./Pages/Header/Header";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import NotFound from "./Pages/NotFound/NotFound";
+import Registration from "./Pages/Registration/Registration";
 
 function App() {
   return (
     <div>
-      <h2>Hello World</h2>
-      <button class="btn">Button</button>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/registration" element={<Registration></Registration>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
