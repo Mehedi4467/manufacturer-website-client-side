d58547f4aa369a360748e4cc7ecf41259f14484c
Mehedi4467 <mehedi446768@gmail.com>
2022-05-24 03:15:15 +0600

    page not found page design complated

--- src/Pages/Header/Menu/Menu.js
+++ src/Pages/Header/Menu/Menu.js
@@ -36,16 +36,10 @@ const Menu = () => {
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
                     <li><CustomLink to='/'>Home</CustomLink></li>
-                    <li tabindex="0">
-                        <a>
-                            Parent
-                            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-                        </a>
-                        <ul className="p-2">
-                            <li><a>Submenu 1</a></li>
-                            <li><a>Submenu 2</a></li>
-                        </ul>
-                    </li>
+                    {
+                        user && <li><CustomLink to='/dashboard'>Dashboard</CustomLink></li>
+                    }
+
                     {
                         user ? <li onClick={() => signOut(auth)} ><button>Log out</button></li> : <li><CustomLink to='/login'>Login</CustomLink></li>
                     }
