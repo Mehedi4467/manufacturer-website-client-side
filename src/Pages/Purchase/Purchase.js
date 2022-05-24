a098d0ffed12bf6b0f87534595b492e32a2912be
Mehedi4467 <mehedi446768@gmail.com>
2022-05-24 07:13:03 +0600

    order function complated

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -11,7 +11,7 @@ const Purchase = () => {
     const { id } = useParams();
     const [user, loading] = useAuthState(auth);
     const [products] = useProducts(id);
-    const { name, description, image, price, minQuantity, quantity } = products;
+    const { name, description, image, price, minQuantity, quantity, _id } = products;
     const [quantityLimite, setQuantityLimite] = useState('true')
 
     if (loading) {
@@ -33,44 +33,80 @@ const Purchase = () => {
         setQuantityLimite(true);
     }
 
+
+    const handelOrder = (event) => {
+        event.preventDefault();
+        setQuantityLimite(false);
+        const orderQuentity = event.target.quantity.value;
+        const customerName = user?.displayName;
+        const email = user?.email;
+        const phone = event.target.phone.value;
+        const address = event.target.address.value;
+        const productId = _id;
+
+        const customerOrder = {
+            orderQuentity, customerName, email, phone, address, productId
+        };
+
+
+        fetch('http://localhost:5000/order', {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(customerOrder)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.acknowledged) {
+                    toast('Your order has been successfully');
+                    setQuantityLimite(true);
+                }
+            })
+
+
+    }
+
+
     return (
         <div>
-            <div class="hero  my-4 bg-base-200">
-                <div class="hero-content flex-col lg:flex-row">
+            <div className="hero  my-4 bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
                     <img src={image} width="400" alt={name} />
-
-                    <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
-                        <div>
-                            <h1 class="text-3xl text-center text-[#FB4010] font-bold">{name}</h1>
-                            <p class="py-6 px-4 text-slate-500">{description}</p>
-                            <div className='shadow-lg text-center px-6 py-4 rounded-full'>
-                                <p className='text-[#FB4010] font-bold'>Available quantity: {quantity}</p>
-                                <p className='text-[#FB4010] font-bold'>Minimum order quantity: {minQuantity}</p>
-
-                                <p className='text-[#FB4010] font-bold'>Price (per unit price): {price}</p>
-                            </div>
-                            <div className='mt-4'>
-                                <label class="label">
-                                    <span class="label-text text-orange-600 font-bold">Product Quantity</span>
-
-                                </label>
-                                <input onChange={(e) => handelQuantity(e.target.value)} type="number" max={quantity} min={minQuantity} name='address' defaultValue={minQuantity} class="input input-bordered input-warning w-full mb-4" required />
-                            </div>
-                        </div>
-                        <form>
+                    <form onSubmit={handelOrder}>
+                        <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
                             <div>
-                                <h1 class="text-3xl text-center text-[#FB4010] font-bold mb-6">User Information</h1>
+                                <h1 className="text-3xl text-center text-[#FB4010] font-bold">{name}</h1>
+                                <p className="py-6 px-4 text-slate-500">{description}</p>
+                                <div className='shadow-lg text-center px-6 py-4 rounded-full'>
+                                    <p className='text-[#FB4010] font-bold'>Available quantity: {quantity}</p>
+                                    <p className='text-[#FB4010] font-bold'>Minimum order quantity: {minQuantity}</p>
+
+                                    <p className='text-[#FB4010] font-bold'>Price (per unit price): {price}</p>
+                                </div>
+                                <div className='mt-4'>
+                                    <label className="label">
+                                        <span className="label-text text-orange-600 font-bold">Product Quantity</span>
+
+                                    </label>
+                                    <input onChange={(e) => handelQuantity(e.target.value)} type="number" max={quantity} min={minQuantity} name='quantity' defaultValue={minQuantity} className="input input-bordered input-warning w-full mb-4" required />
+                                </div>
+                            </div>
+
+                            <div>
+                                <h1 className="text-3xl text-center text-[#FB4010] font-bold mb-6">User Information</h1>
 
                                 <div>
-                                    <input type="text" defaultValue={user.displayName} placeholder="Name" class="input input-bordered input-warning w-full mb-4 " readOnly />
-                                    <input type="text" defaultValue={user.email} placeholder="Name" class="input input-bordered input-warning w-full mb-4 " readOnly />
-                                    <input type="text" name='phone' placeholder="Your Phone Number" class="input input-bordered input-warning w-full mb-4" required />
-                                    <input type="text" name='address' placeholder="Your Address" class="input input-bordered input-warning w-full mb-4" required />
+                                    <input type="text" defaultValue={user.displayName} placeholder="Name" className="input input-bordered input-warning w-full mb-4 " readOnly />
+                                    <input type="text" defaultValue={user.email} placeholder="Name" className="input input-bordered input-warning w-full mb-4 " readOnly />
+                                    <input type="text" name='phone' placeholder="Your Phone Number" className="input input-bordered input-warning w-full mb-4" required />
+                                    <input type="text" name='address' placeholder="Your Address" className="input input-bordered input-warning w-full mb-4" required />
                                 </div>
-                                <button class="btn bg-[#FB4010] w-full" disabled={!quantityLimite}>Place Order</button>
+                                <button className="btn bg-[#FB4010] w-full" disabled={!quantityLimite}>Place Order</button>
                             </div>
-                        </form>
-                    </div>
+
+                        </div>
+                    </form>
 
                 </div>
             </div>
