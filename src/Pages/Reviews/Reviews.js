98ea83f73deab4f8448fa918364c550b71e77dd2
Mehedi4467 <mehedi446768@gmail.com>
2022-05-26 22:26:28 +0600

    some change in review section

--- src/Pages/Reviews/Reviews.js
+++ src/Pages/Reviews/Reviews.js
@@ -1,8 +1,20 @@
 import Carousel from 'nuka-carousel';
 import React from 'react';
+import { useQuery } from 'react-query';
+import Spinner from '../Spinner/Spinner';
 
 
 const Reviews = () => {
+
+    const { isLoading, data } = useQuery('review', () =>
+        fetch('https://lit-mountain-23720.herokuapp.com/review').then(res =>
+            res.json()
+        )
+    )
+
+    if (isLoading) {
+        return <Spinner></Spinner>
+    }
     return (
         <div className='container mx-auto my-10'>
             <h2 className='text-3xl text-center pt-10 font-bold text-slate-400'>Customer Reviews</h2>
@@ -28,50 +40,20 @@ const Reviews = () => {
                     }}
                 >
 
-                    <div>
-                        <h2 className="text-xl text-center mb-2 text-orange-500">Md.Mehedi Hassan</h2>
-                        <p className='text-slate-500 px-4'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                        <div className="rating flex justify-center pt-2 pb-10">
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                        </div>
-                    </div>
-                    <div>
-                        <h2 className="text-xl text-center mb-2 text-orange-500">Md.Mehedi Hassan</h2>
-                        <p className='text-slate-500 px-4'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                        <div className="rating flex justify-center pt-2 pb-10">
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                        </div>
-                    </div>
-                    <div>
-                        <h2 className="text-xl text-center mb-2 text-orange-500">Md.Mehedi Hassan</h2>
-                        <p className='text-slate-500 px-4'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                        <div className="rating flex justify-center pt-2 pb-10">
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                        </div>
-                    </div>
-                    <div>
-                        <h2 className="text-xl text-center mb-2 text-orange-500">Md.Mehedi Hassan</h2>
-                        <p className='text-slate-500 px-4'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.</p>
-                        <div className="rating flex justify-center pt-2 pb-10">
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                            <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-                        </div>
-                    </div>
+                    {
+                        data?.map(reviewData => <div key={reviewData._id}>
+                            <h2 className="text-xl text-center mb-2 text-orange-500">{reviewData.name} </h2>
+                            <p className='text-slate-500 px-4'>{reviewData.review}</p>
+                            <div className="rating flex justify-center pt-2 pb-10">
+
+                                {
+                                    [...Array(parseInt(reviewData.rating))].map(rating => <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />)
+                                }
+
+                            </div>
+                        </div>)
+                    }
+
                 </Carousel>
             </div>
         </div>
